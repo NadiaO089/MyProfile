10aaf81d1cc3d16009946bf4c740e20ac0d74103
NadiaO089 <nadiaolivera@gmail.com>
2023-01-23 11:10:44 -0300

    Update

--- src/Components/ContactMe/ContactMe.jsx
+++ src/Components/ContactMe/ContactMe.jsx
@@ -18,8 +18,8 @@ const ContactMe = () => {
         <h4 className={s.text}>Contáctame</h4>
         <div >
          <input  className={s.button} type='button' onClick={e => onClick(e)}/>
-         <a className={s.icons} href='https://www.linkedin.com/in/nadia-olivera-a4832791/'><img src='https://cdn-icons-png.flaticon.com/512/3955/3955051.png' alt='LinkesIn'  width='35em' height='35em'/></a>
-         <a className={s.icons} href='https://github.com/NadiaO089'><img src='https://cdn-icons-png.flaticon.com/128/1051/1051326.png' alt='GitHub'  width='35em' height='35em'/></a>
+         <a target='_blanck' className={s.icons} href='https://www.linkedin.com/in/nadia-olivera-a4832791/'><img src='https://cdn-icons-png.flaticon.com/512/3955/3955051.png' alt='LinkesIn'  width='35em' height='35em'/></a>
+         <a target='_blanck' className={s.icons} href='https://github.com/NadiaO089'><img src='https://cdn-icons-png.flaticon.com/128/1051/1051326.png' alt='GitHub'  width='35em' height='35em'/></a>
         {flag? <p className={s.text}>nadiaolivera@gmail.com</p> : null} 
         </div>
         </div>
--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -9,13 +9,13 @@ const Proyects = () => {
            <h2>Proyectos</h2>
            <div className={s.container1}>
            <div>
-           <a href='https://github.com/NadiaO089/MyVideogamesApp'><video className={s.video} loop autoPlay muted>
+           <a target='_blanck' href='https://github.com/NadiaO089/MyVideogamesApp'><video className={s.video} loop autoPlay muted>
           <source src={proyecto1} type="video/mp4" />
           </video>
          </a>
            </div>
            <div>
-           <a href='https://changapp.up.railway.app/'><video className={s.video} loop autoPlay muted>
+           <a target='_blanck' href='https://changapp.up.railway.app/'><video className={s.video} loop autoPlay muted>
           <source src={proyecto2} type="video/mp4" />
           </video>
          </a>
